295b5946a2b12feebeafb4af60970d181c47a991
Ashwinipujari14 <102610850+Ashwinipujari14@users.noreply.github.com>
2022-06-08 10:54:58 +0530

    Binary search tree program
    Using an node i develeoped a binary search tree program using binary search tree logic

--- Main.java
+++ Main.java
@@ -2,22 +2,50 @@
 public class Main {
 
 	public static void main(String[] args) {
-	
-		Binary b = new Binary(10);
+		// TODO Auto-generated method stub
+
+		BST bst = new BST();
 		
-		b.accept();
-		b.bin_search(21);
-		int pos =  b.bin_search(21);
-		if(pos!=-1)
-		{
-			System.out.println("Found at "+pos+" position");
+		bst.insert(23);
+		bst.insert(12);
+		bst.insert(3);
+		bst.insert(89);
+		bst.insert(90);
+		bst.insert(67);
+		bst.insert(35);
+		bst.insert(39);
+		bst.insert(34);
+		bst.insert(27);
+		bst.insert(19);
+		bst.insert(25);
+		//bst.printT();
+		//System.out.println(bst);
+		
+		System.out.println("..............PreOrder..........");
 
-		}
-		else
-		{
-			System.out.println("Not found");
-		}
-
+		bst.preOrder();
+		System.out.println();
+		System.out.println("..............Inorder..........");
+		
+		bst.InOrder();
+		System.out.println();
+		System.out.println("..............PostOrder..........");
+		bst.postOrder();
+		System.out.println();
+		System.out.println(".........Largest and smallest.....");
+		bst.print_largest();
+		bst.print_secondLargest();
+		
+		bst.print_smallest();
+		bst.print_secondsmallest();
+	
+		bst.delete(12);
+		bst.InOrder();
+		System.out.println();
+		bst.delete(46);
+		bst.InOrder();
+		
+		bst.print_H();
 	}
 
 }
