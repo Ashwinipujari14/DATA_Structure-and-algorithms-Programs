699b6f47c6881a93b0ed12e916d3dc71b12cf298
Ashwinipujari14 <102610850+Ashwinipujari14@users.noreply.github.com>
2022-06-13 00:29:43 +0530

    Add files via upload

--- Pog/Main.java
+++ Pog/Main.java
@@ -4,13 +4,24 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Dquque dQueue = new Dquque();
+		Queue q1 = new Queue();
 		
-		dQueue.insert_R(10);
-		dQueue.insert_R(20);
-		dQueue.insert_F(30);
-		System.out.println(dQueue);
+		q1.insert(100);
+		q1.insert(300);
+		q1.insert(500);
+		q1.remove();
 		
+	//	q1.insert(700);
+	//	q1.insert(800);
+		//q1.insert(900);
+		System.out.println(q1);
+		
+		Queue q2 = new Queue();
+		q2.insert(1000);
+		q2.insert(5000);
+		q2.insert(3);
+		q2.insert(4);
+		System.out.println(q2);
 	}
 
 }
